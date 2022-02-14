a16062d54abe0736f097f4d6ff8ad1edf7ff13df
MatheusSantos007 <80589583+MatheusSantos007@users.noreply.github.com>
2022-02-14 15:20:41 -0300

    Update cardapio.js
    cardapio.js

--- LIS-main/CafeAppLis/js/cardapio.js
+++ LIS-main/CafeAppLis/js/cardapio.js
@@ -1,12 +1,15 @@
 import itens from './model/dataset.js';
-import foodsModel from './model/foods.js';
+import foodsModel from './model/foods.js';// aqui estamos importando os itens do script dataset e foods
+
+//Para que ao adicionar um novo item no meu cardápio, eu não precise ficar inserido novos conjunto de códigos (cards) no html,
+// trazemos a estrutura do card aqui, para o js e utilizamos uma estrutura de repetição.
 
 foodsModel.load(itens);
 let foods = foodsModel.readAll();
 
 function initFoodsCard () {
-  
-  for (let item of foods) {
+  // o for fará a repetição dos itens do cardápio
+  for (let item of foods) {// e também colocamos o item."elemento". , dentro de uma string literals para que apareça o elemento especificado da nossa lista "ITENS".
 
     const view = createFoodCardItem(item);
   
@@ -17,7 +20,8 @@ function initFoodsCard () {
 }
 
 function createFoodCardItem (item) {
-
+ // foi definida uma variável view, para que posteriormente, possa ser adicionada na tela
+  //essa DIV é a estrutura do nosso card.
     const view = `<div class="col-3 card my-1 mx-1 py-1">
                     <img src="${item.imagem}" class="card-img-top" alt="...">
   
@@ -42,10 +46,17 @@ foodForm.onsubmit = function (event) {
 
   let newFood = Object.fromEntries(new FormData(foodForm));
   foodsModel.create(newFood);
-
+//let itensCardapio = document.querySelector('.itens-cardapio'); // esta é uma outra forma de selecionar o elemento do html
   const foodCard = createFoodCardItem(newFood);
-  let itensCardapio = document.getElementById("itens-cardapio");
-  itensCardapio.insertAdjacentHTML('beforeend', foodCard);
+  let itensCardapio = document.getElementById("itens-cardapio");// definimos uma variável para os itens do cardápio e utilizamos o elemento getElementById, para que possa ser selecionado o elemento da div que contém o card.
+  itensCardapio.insertAdjacentHTML('beforeend', foodCard);// por fim, é utilizado o itens.Cardapio com o elemento insertAdJacentHTML, que tem a função de fazer com que o item apareça na tela, em seguida nos parentes tem "beforeend", que significa dizer, aparecer antes do fim, o view é o código que será inserido..
+
+}
+// resumidamente, este script fará com que os itens do nosso array, apareça nos espaços especificados nque estão contidos
+// na variável view, e para que de fato os códigos que estão nesta variável, sejam inseridos, pegamos o id do elemento lá
+// no html e inserimos através do insertAdjacentHTML. a cada vez que adicionamos itens lá no array, eles serão executados
+// já que declaramos uma estrutura de repetição, o for.
+
 }
 
 initFoodsCard();
